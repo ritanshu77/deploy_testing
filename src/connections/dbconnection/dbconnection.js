3bfd855e50ad488678bbc056382d40fa0fd93cb4
Ritanshu patwa <rpchhipa@gmail.com>
2022-09-03 15:15:20 +0530

    env remove

--- src/connections/dbconnection/dbconnection.js
+++ src/connections/dbconnection/dbconnection.js
@@ -1,10 +1,10 @@
 const mongoose=require("mongoose");
-
-mongoose.connect(`${process.env.DB_URL}`,{
+let DB_URL=process.env.DB_URL || "mongodb+srv://ritanshu:2eexGt8yksbxIDCD@cluster0.hrnhm3t.mongodb.net/deploying_test"
+mongoose.connect(`${DB_URL}`,{
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(()=>{
-    console.log(`dbconnection successfully on ${process.env.DB_URL}`)
+    console.log(`dbconnection successfully on ${DB_URL}`)
 }).catch((e)=>{
     console.log('-->>DB CONNECTION ERROR---->>',e)
 });
